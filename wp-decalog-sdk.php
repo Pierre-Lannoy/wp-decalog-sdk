8c32fbc10eacbc88905a9d2f4c6450c5899172ff
Pierre Lannoy <pierre@lannoy.fr>
2021-11-11 20:47:01 +0100

    Update credits and links.

--- wp-decalog-sdk.php
+++ wp-decalog-sdk.php
@@ -2,13 +2,13 @@
 /**
  * @wordpress-plugin
  * Plugin Name:       WordPress DecaLog SDK Demo
- * Plugin URI:        https://github.com/Pierre-Lannoy/wp-decalog-sdk
+ * Plugin URI:        https://perfops.one/decalog-sdk
  * Description:       Simple demo plugin to test WordPress DecaLog SDK.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      5.6
- * Author:            Pierre Lannoy
- * Author URI:        https://pierre.lannoy.fr
+ * Author:            PerfOps One / Pierre Lannoy
+ * Author URI:        https://perfops.one
  * License:           GPLv3
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       wp-decalog-sdk
@@ -19,7 +19,7 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-
+/*
 require_once __DIR__ . '/vendor/autoload.php';
 
 $slug = 'my-plugin-slug';
@@ -28,25 +28,37 @@ $slug = 'my-plugin-slug';
 
 
 
-/*$events  = \DecaLog\Engine::eventsLogger( $slug );
+$events  = \DecaLog\Engine::eventsLogger( $slug );
 $metrics = \DecaLog\Engine::metricsLogger( $slug );
 $traces  = \DecaLog\Engine::tracesLogger( $slug );
 
-$sleep = $traces->start_span( 'test bootstrap' );
-$span  = $traces->start_span( 'subtest bootstrap', $sleep );
+$sleep = $traces->startSpan( 'test bootstrap', DECALOG_SPAN_PLUGINS_LOAD );
+$span  = $traces->startSpan( 'subtest bootstrap', $sleep );
 usleep( 200000 );
-$traces->end_span( $span );
+$traces->endSpan( $span );
 usleep( 50000 );
-$traces->end_span( $sleep );
+$traces->endSpan( $sleep );
 
-
+add_filter(
+			'decalog_events_list_actions_for_ip',
+			function( $actions, $item ) {
+				$actions[] = [
+					'url'  => 'https://google.fr',
+					'hint' => $item['message'],
+					'icon' => 'compass',
+				];
+				return $actions;
+			},
+			10,
+			2
+		);
 
 add_action(
 	'shutdown',
 	function() use ( &$traces ) {
-		$sleep = $traces->start_span( 'test shutdown' );
+		$sleep = $traces->startSpan( 'test shutdown', DECALOG_SPAN_SHUTDOWN );
 		usleep( 100000 );
-		$traces->end_span( $sleep );
+		$traces->endSpan( $sleep );
 	},
 	10,
 	0
@@ -55,15 +67,15 @@ add_action(
 add_action(
 	'wp_head',
 	function() use ( &$traces ) {
-		$sleep = $traces->start_span( 'test wp_head' );
-		$s     = $traces->start_span( 'subtest wp_head', $sleep );
+		$sleep = $traces->startSpan( 'test wp_head', DECALOG_SPAN_MAIN_RUN );
+		$s     = $traces->startSpan( 'subtest wp_head', $sleep );
 		usleep( 100000 );
-		$traces->end_span( $s );
+		$traces->endSpan( $s );
 		usleep( 50000 );
-		$traces->end_span( $sleep );},
+		$traces->endSpan( $sleep );},
 	10,
 	0
-);
-*/
+);*/
+
 
 
